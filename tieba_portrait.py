34b749218d128be8c6324ee1966cf24599fb5171
Gazali <yuujinnx@gmail.com>
2016-09-14 12:19:29 +0800

    rewrite

--- tieba_portrait.py
+++ tieba_portrait.py
@@ -1,37 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 '''
 百度贴吧用户头像原图爬虫（以该吧经验值从高到低开始爬）
 '''
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
 import os
-def download_usr_pic(url, name):#获取并下载用户头像
-	html = urlopen(url)
-	bs = BeautifulSoup(html.read(), 'lxml')
-	pics = bs.findAll('a', {'class':'userinfo_head'})[0]
-	for pic in pics:
-		pic_url = r'http://himg.baidu.com/sys/portraitl/item/'+pic['src'].split('/')[-1].split('?')[0]#头像原图地址
-		with open(name+'.jpg', 'wb') as fout:
-			fout.write(urlopen(pic_url).read())
-			print(name, '....ok')
+import shutil
+import requests
+from lxml import etree
 
-def get_usr_url(url):#获取url页面上所有用户的个人主页地址
-	html = urlopen(url)
-	bs = BeautifulSoup(html.read(), 'lxml')
-	usrs = bs.findAll('a', {'class':'drl_item_name_top'}) + bs.findAll('a', {'class':'drl_item_name_nor'})
-	for usr in usrs:
-		download_usr_pic(r'http://tieba.baidu.com'+usr['href'], usr.get_text())
+def download_usr_pic(url, name): # 获取并下载用户头像
+    print('Getting %s...' % name)
+    page = requests.get(url)
+    html = etree.HTML(page.text)
+    try:
+        pic_url = html.xpath('//*[@id="j_userhead"]/a/img/@src')[0]
+    except: # 部分用户主页无法访问，提示页面不存在
+        print('404 Error!')
+        return
+    pic_url = r'http://himg.baidu.com/sys/portraitl/item/' + pic_url.split('/')[-1]
+    res = requests.get(pic_url, stream = True)
+    with open(name + '.jpg', 'wb') as fout:
+        shutil.copyfileobj(res.raw, fout)
 
-dir = 'tieba_portrait'
-try:
-	os.mkdir(dir)
-except:
-	pass
-os.chdir(dir)
-tieba_name = input('Please input the name of tieba...\n')
-tieba_name = str(tieba_name.encode('GB2312')).replace(r'\x','%').split("'")[1]#进行网址的GB2312编码
-pages = int(input('How many pages do you want?\n'))
-url = r'http://tieba.baidu.com/f/like/furank?kw=' + tieba_name + '&pn=';
-for i in range(1, pages+1):
-	get_usr_url(url+str(i))
+def get_usr_url(url): # 获取url页面上所有用户的个人主页地址
+    page = requests.get(url)
+    html = etree.HTML(page.text)
+    usr_lists = html.xpath('//a[@class="drl_item_name_top"] | //a[@class="drl_item_name_nor"]')
+    for usr in usr_lists:
+        download_usr_pic(r'http://tieba.baidu.com'+usr.get('href'), usr.text)
+
+if __name__ == '__main__':
+    tieba_name = input('Please input the name of tieba...\n')
+    dir = os.path.join(os.getcwd(), 'tieba_portrait', tieba_name)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    os.chdir(dir)
+    tieba_name = str(tieba_name.encode('GB2312')).replace(r'\x','%').split("'")[1]#进行网址的GB2312编码
+    pages = int(input('How many pages ?\n'))
+    url = r'http://tieba.baidu.com/f/like/furank?kw=' + tieba_name + '&pn=';
+    for i in range(1, pages+1):
+        get_usr_url(url+str(i))
